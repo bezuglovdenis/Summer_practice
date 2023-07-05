e8073cb2464cb682545a76cd1d2036489108f0b0
bezuglovdenis <bezuglov200395@gmail.com>
2023-07-05 20:28:46 +0600

    commit

---
+++ task2/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Timer from './timer';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Таймер</h1>
+      <Timer />
+    </div>
+  );
+};
+
+export default App;
